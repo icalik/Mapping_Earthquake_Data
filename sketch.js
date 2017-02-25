1a5da2886065f5d6db6db65d4eb7e492421d6e8a
Ismet Said Calik <ismetsaid.calik@gmail.com>
2017-02-25 19:56:16 +0300

    Done

--- sketch.js
+++ sketch.js
@@ -1,47 +1,63 @@
 var mapimg;
 
-
 var clat = 0;
 var clon = 0;
 
+var ww = 1024;
+var hh = 512;
+
 var zoom = 1;
-
-var lat = 38.67431;
-var lon = 39.22321;
-
+var earthquakes;
 
 function preload() {
-  mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/0,0,1,0,0/1024x512?access_token=pk.eyJ1IjoiaXNjYWxpayIsImEiOiJjaXpsZjBsb3AwMDBzMndtZm1ucWJmdWE4In0.imQhhgVvyQ2lf6i6xu0EOQ');
+  mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/' +
+    clat + ',' + clon + ',' + zoom + '/' +
+    ww + 'x' + hh +
+    '?access_token=pk.eyJ1IjoiaXNjYWxpayIsImEiOiJjaXpsZjBsb3AwMDBzMndtZm1ucWJmdWE4In0.imQhhgVvyQ2lf6i6xu0EOQ');
+  // earthquakes = loadStrings('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.csv');
+  earthquakes = loadStrings('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv');
 }
 
 function mercX(lon) {
   lon = radians(lon);
-  var a = (256/PI) * pow(2,zoom);
+  var a = (256 / PI) * pow(2, zoom);
   var b = lon + PI;
-  return a*b;
+  return a * b;
 }
 
 function mercY(lat) {
   lat = radians(lat);
-  var a = (256/PI) * pow(2,zoom);
-  var b = tan(PI/4 + lat/2);
-  var c = PI - log(b); //This log is ln, natural logarithm.
-  return a*c;
+  var a = (256 / PI) * pow(2, zoom);
+  var b = tan(PI / 4 + lat / 2);
+  var c = PI - log(b);
+  return a * c;
 }
 
+
 function setup() {
-  createCanvas(1024,512);
-  translate(width/2,height/2);
+  createCanvas(ww, hh);
+  translate(width / 2, height / 2);
   imageMode(CENTER);
-  image(mapimg,0,0);
+  image(mapimg, 0, 0);
 
   var cx = mercX(clon);
   var cy = mercY(clat);
 
-  var x = mercX(lon) - cx;
-  var y = mercY(lat) - cy;
-
-  fill(255,0,255,200);
-  ellipse(x,y,8,8);
+  for (var i = 1; i < earthquakes.length; i++) {
+    var data = earthquakes[i].split(/,/);
+    //console.log(data);
+    var lat = data[1];
+    var lon = data[2];
+    var mag = data[4];
+    var x = mercX(lon) - cx;
+    var y = mercY(lat) - cy;
+    mag = pow(10, mag);
+    mag = sqrt(mag);
+    var magmax = sqrt(pow(10, 10));
+    var d = map(mag, 0, magmax, 0, 180);
+    stroke(255, 0, 255);
+    fill(255, 0, 255, 200);
+    ellipse(x, y, d, d);
+  }
 
 }
